97e89a16f1345698c73df246eb61721fe53401a0
Ron Gierlach <rongierlach@gmail.com>
2017-01-31 20:03:34 -0600

    prepare to start api as well as server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const bundlePaths = require('./.bundle-paths.json')
+// const api = require(bundlePaths.api)
 const server = require(bundlePaths.server)
 
 // let's roll
+// api.start()
 server.start()
