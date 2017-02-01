ec74ff79a012e268e571a0ef1184ca461b6171b7
Ron Gierlach <rongierlach@gmail.com>
2017-01-31 20:00:58 -0600

    prepare webpack for separate api config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,9 @@
+// import api from './webpack/config.api.js'
 import client from './webpack/config.client.js'
 import server from './webpack/config.server.js'
 
 export default [
+  // api,
   client,
   server
 ]
---
+++ webpack/config.api.js
@@ -0,0 +1,47 @@
+import config from '../config'
+import { resolve } from 'path'
+import { makeRule } from '../tools/helpers'
+
+import LogCompilerErrorsPlugin from './plugins/logCompilerErrors'
+import WriteBundlePathsToJSONPlugin from './plugins/WriteBundlePathsToJSON'
+
+const DEBUG = config.env !== 'production'
+
+export default {
+  entry: {
+    api: resolve(__dirname, '../src/api/index.js')
+  },
+  output: {
+    path: './bundles',
+    pathinfo: DEBUG,
+    publicPath: './public',
+    libraryTarget: 'umd',
+    umdNamedDefine: false,
+    filename: `[name].js`
+  },
+  target: 'node',
+  module: {
+    rules: [
+      makeRule(/\.jsx?$/, 'standard-loader', 'pre'),
+      makeRule(/\.jsx?$/, 'babel-loader')
+    ]
+  },
+  resolve: {
+    alias: {
+      config$: resolve(__dirname, '../config'),
+      logger$: resolve(__dirname, '../tools/logger.js'),
+      helpers$: resolve(__dirname, '../tools/helpers.js')
+    }
+  },
+  plugins: [
+    new LogCompilerErrorsPlugin(),
+    new WriteBundlePathsToJSONPlugin()
+  ],
+  stats: {
+    chunks: true, // Makes the build much quieter
+    colors: true, // Shows colors in the console
+    hash: true,
+    version: false,
+    timings: true
+  }
+}
